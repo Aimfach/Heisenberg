dcad3a46126845363a1ec33947066f3eb30f5c3b
Aimfach <anthony.lanzenberger@gmail.com>
2022-09-17 10:12:04 +0200

    test

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
 print("hi")
 p = GPIO.PWM(23, 1000)
 g = GPIO.PWM(24, 1000)
+u = GPIO.PWM(26, 1000)
+a = GPIO.PWM(12, 1000)
+i = GPIO.PWM(17, 1000)
 p.start(0)
 g.start(0)
+u.start(0)
+a.start(0)
+i.start(0)
 
 
 
@@ -24,12 +33,18 @@ def set_power(power):
     power = int(power)
     try:
         p.ChangeDutyCycle(power)
-        g.ChangeDutyCycle(power)
+        g.ChangeDutyCycle(power/2)
+        u.ChangeDutyCycle(power-10)
+        a.ChangeDutyCycle(power-30)
+        i.ChangeDutyCycle(power/3)
         print(power)                
     except KeyboardInterrupt:
         pass
         p.stop()
         g.stop()
+        u.stop()
+        a.stop()
+        i.stop()
         GPIO.cleanup()
     
 
